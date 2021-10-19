df40432c16c71741eca6cebf1dfabd7f7a388dac
celinaidir <45858161+celinaidir@users.noreply.github.com>
2021-10-19 18:59:37 +0200

    Create Model.java

---
+++ Model.java
@@ -0,0 +1,19 @@
+package fr.dauphine.ja.CelinaIdir.shapes.model;
+
+import java.util.*;
+
+public class Model {
+	
+	public static List<Circle> shapes = new ArrayList<Circle>();
+	
+	public void add(Circle c) { //ajout d'un cercle
+		shapes.add(c);
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
