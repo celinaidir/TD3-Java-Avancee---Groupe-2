012af927ba3f24f7bef9b39f69c00b98c8c80646
celinaidir <45858161+celinaidir@users.noreply.github.com>
2021-10-19 19:02:34 +0200

    Create MouseController.java

---
+++ MouseController.java
@@ -0,0 +1,12 @@
+package fr.dauphine.ja.CelinaIdir.shapes.Controller;
+
+import java.awt.event.MouseEvent;
+
+public class MouseController implements java.awt.event.MouseMotionListener {
+
+	@Override
+	public void mouseDragged(MouseEvent e) {}
+	@Override
+	public void mouseMoved(MouseEvent e) {}
+
+}
